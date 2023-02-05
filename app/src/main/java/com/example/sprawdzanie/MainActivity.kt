4c78d8c5c00f11496e2bfc73d4f86f181ea2d0c8
turcio <arturgunter2006@gmail.com>
2023-02-05 18:04:31 +0100

    zgodnosc

--- app/src/main/java/com/example/sprawdzanie/MainActivity.kt
+++ app/src/main/java/com/example/sprawdzanie/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.sprawdzanie
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.CheckBox
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+       val guzior=findViewById<Button>(R.id.SubmitButton)
+        val password=findViewById<EditText>(R.id.password)
+        val passwords=findViewById<EditText>(R.id.passwords)
+
+
+        guzior.setOnClickListener{
+            val check=findViewById<CheckBox>(R.id.CheckZgodny)
+            if(password.text.toString()!=passwords.text.toString()){
+                check.isChecked=false
+            }else{
+                check.isChecked=true
+
+            }
+        }
     }
 }
